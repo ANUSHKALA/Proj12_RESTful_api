0ab29aaa13f9d0e08b8d3a150640a845861c262a
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-25 16:18:59 +0530

    get post delete

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+    title : String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema)
+
+
+app.route("/articles")
+.get(
+    function (req,res){
+        Article.find({},function (err, foundDoc){
+            if(!err){
+                res.send(foundDoc);
+            }
+            else{
+                res.send(err);
+            }
+        })
+    }
+)
+.post(
+    function (req,res){
+        console.log(req.body.title);
+        console.log(req.body.content);
+    
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content,
+        })
+        newArticle.save(function (err){
+            if(!err){
+                console.log("Logged successfully");
+            }
+            else{
+                console.log(err)
+            }
+        });
+    }
+)
+.delete(
+    function (req,res){
+        Article.deleteMany(function (err){
+            if(!err){
+                console.log("a;; items deleted successfully!");
+            }
+            else{
+                console.log(err);
+            }
+        })
+    }
+);
+
+app.listen(3000,function(){
+    console.log("This server is active on port 3000.");
+})
